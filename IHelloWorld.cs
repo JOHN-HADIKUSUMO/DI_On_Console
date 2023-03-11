3a3760e1d889f2429be83ab8957c20b836ac656d
John Hadikusumo <john.hadikusumo@efficientia.com.au>
2023-03-11 15:31:19 +1100

    Add project files.

---
+++ IHelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_On_Console
+{
+    public interface IHelloWorld
+    {
+
+        public string GetString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI_On_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped<IHelloWorld, HelloWorld>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+            IHelloWorld instance = provider.GetService<IHelloWorld>();
+            string value = instance.GetString();
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
